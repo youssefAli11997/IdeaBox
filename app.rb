37152ab9af662f988242255e1da5b6bb0602c1b1
Youssef Ali <youssefali6212@gmail.com>
2019-06-24 23:41:03 +0200

    recording ideas

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
+require './idea'
+
 class IdeaBoxApp < Sinatra::Base
-  get '/' do
- 	 "Hello, World!"
-  end
+	not_found do
+	    erb :error
+	  end
+
+	configure :development do
+		register Sinatra::Reloader
+	end
+
+
+	get '/' do
+		erb :index
+	end
+
+	post '/' do
+		idea = Idea.new
+		idea.save
+	end
 
 end
